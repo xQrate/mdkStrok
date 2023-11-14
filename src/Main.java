c4dedac4d3bbaf35fadbc17bcb753ef150de29dd
xQrate <daniilsteponenko942@gmail.com>
2023-11-14 15:32:49 +0300

    ver2

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         try( FileWriter fw= new FileWriter("C://Users//Даниил//Desktop//tezt//files.txt")) {
-            System.out.println("Слова");
-            String text = "Hello \ngods";
 
+            String text = "Hello gods";
+            String[] arr = text.split(" ");
+            System.out.println("Кол-во слов");
+            System.out.println(arr.length);
+            char ch = 'l';
+            int count = text.length() - text.replace(String.valueOf(ch), "").length();
             fw.write(text);
             fw.append('\n');
             fw.flush();
+
+            System.out.println("символы"+ '\n'+ text.length() );
+            System.out.println("Слова");
         }
         catch (IOException ex) {
             System.out.println(ex.getMessage());
@@ -29,6 +36,7 @@ public class Main {
         }
         catch (IOException ex){
             System.out.println(ex.getMessage());
+
         }
 
     }
